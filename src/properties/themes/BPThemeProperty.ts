58f0ace1dc332deab51dc5b61bebcbf4b1f717af
AlfredYang1986 <yangyuanpig@gmail.com>
2019-10-10 17:26:05 +0800

    绘制context抽象

--- src/properties/themes/BPThemeProperty.ts
+++ src/properties/themes/BPThemeProperty.ts
@@ -7,17 +7,17 @@ export class BPThemeProperty {
 
     // TODO: 修改配置文件
     protected normals: Array<{key: string, v: any}> = [
-        { key: "height", v: 100},
-        { key: "width", v: 100},
-        { key: "margin-left", v: 8},
-        { key: "margin-right", v: 8},
-        { key: "margin-top", v: 8},
-        { key: "margin-bottom", v: 8},
-        { key: "padding-left", v: 8},
-        { key: "padding-right", v: 8},
-        { key: "padding-top", v: 8},
-        { key: "padding-bottom", v: 8},
-        { key: "background", v: "#FFFFFF"},
+        { key: "height", v: "100px" },
+        { key: "width", v: "100px" },
+        { key: "margin-left", v: 8 },
+        { key: "margin-right", v: 8 },
+        { key: "margin-top", v: 8 },
+        { key: "margin-bottom", v: 8 },
+        { key: "padding-left", v: 8 },
+        { key: "padding-right", v: 8 },
+        { key: "padding-top", v: 8 },
+        { key: "padding-bottom", v: 8 },
+        { key: "background", v: "#FFFFFF" },
     ]
 
     constructor() {
