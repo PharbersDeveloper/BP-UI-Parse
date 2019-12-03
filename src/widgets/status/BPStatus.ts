0cc63926b2b788b02576e06fc0b52e288f79f0b5
fuhanyi <simonzoo@yeah.net>
2019-12-03 17:14:01 +0800

    status组件 样式数据状态提取

--- src/widgets/status/BPStatus.ts
+++ src/widgets/status/BPStatus.ts
@@ -7,6 +7,8 @@ import phLogger from "../../logger/phLogger"
 import { IOptions } from "../../properties/Options"
 import { BPWidget } from "../BPWidget"
 import BPComp from "../Comp"
+import BPSlot from "../slotleaf/BPSlot"
+
 export default class BPStatus extends BPWidget {
     constructor(output: string, name: string, routeName: string) {
             super(output, name, routeName)
@@ -16,6 +18,7 @@ export default class BPStatus extends BPWidget {
 
         const options: IOptions = {
                 comp,
+                hbsData: this.paintHBS(),
                 logicData: this.paintLogic(comp), // js
                 output: this.output,
                 pName: this.projectName,
@@ -35,15 +38,103 @@ export default class BPStatus extends BPWidget {
         const fileDataStart = this.paintLoginStart(comp)
         const fileDataEnd = this.paintLoginEnd()
 
-        const fileData = "\n" +
-            "export default Component.extend({" + "\r" +
-            "    layout," + "\r" +
-            "    tagName:'span'," + "\r" +
-            "    classNames:['" + comp.name + "']," + "\r" +
-            "    content: 'default'," + "\r" +
-            "    classNameBindings: ['block:btn-block', 'reverse', 'active', 'computedIconOnly:icon-only']," + "\r" +
-            "    attributeBindings: ['disabled']," + "\r"
+        const fileData = `
+        import { computed } from '@ember/object';
+        export default Component.extend({
+            layout,
+            tagName:'span',
+            classNames:['${comp.name}'],
+            content: 'default',
+            classNameBindings: ['block:btn-block', 'reverse', 'active', 'computedIconOnly:icon-only', 'currentType', 'subtle:status-subtle:status-bold'],
+            attributeBindings: [''],
+            type: 'default',
+            /**
+             *type:
+             *default/success/removed/in-progress/new/moved
+            */
+            currentType: computed('type', function () {
+                let type = this.get('type');
+
+                return "status-" + type
+            }),
+            click() {
+                let action = this.actions.emit;
+
+                action.call(this, this, "click", "")
+                /**
+                 * other way bind this
+                 * const emit = action.bind(this);
+                 * emit(this,"click","")
+                 */
+                return this.get('bubble');
+            },
+            mouseEnter() {
+                let action = this.actions.emit;
+
+                action.call(this, this, "mouseEnter", "")
+            },
+            mouseLeave() {
+                let action = this.actions.emit;
+
+                action.call(this, this, "mouseLeave", "")
+            },
+            actions: {
+                emit(source, signal, data) {
+                    this.sendAction("emit", source, signal, data)
+                },
+                disconnect(ss, ts, cs) {
+                    this.sendAction("disconnect", ...this.mstc)
+                },
+                ssc(ss, ts, cs) {
+                    const mss = ss
+
+                    mss.pushObject({ "source": this, "signal": "click" })
+                    const mts = ts
+
+                    mts.pushObject({ "target": this, "slot": this.get("actions.slots.onClick") })
+                    const mcs = cs
+
+                    mcs.pushObject({
+                        "source": this,
+                        "signal": "click",
+                        "target": this,
+                        "slot": this.get("actions.slots.onClick")
+                    })
+                    mcs.pushObject({
+                        "source": this,
+                        "signal": "mouseEnter",
+                        "target": this,
+                        "slot": this.get("actions.slots.onMouseEnter")
+                    })
+                    mcs.pushObject({
+                        "source": this,
+                        "signal": "mouseLeave",
+                        "target": this,
+                        "slot": this.get("actions.slots.onMouseLeave")
+                    })
+                    this.set("mstc", [mss, mts, mcs])
+
+                    this.sendAction("ssc", mss, mts, mcs)
+                },
+                slots: {
+                    onClick(target, data) {
+                        alert("BP-UI-Parse click event =>  ${comp.name}" + data)
+                    },
+                    onMouseEnter(target,data) {
+                        window.console.log("BP-UI-Parse mouseEnber event =>  ${comp.name}" + data)
+                    },
+                    onMouseLeave(target,data) {
+                        window.console.log("BP-UI-Parse mouseLeave event =>  ${comp.name}" + data)
+                    }
+                }
+            },`
 
         return fileDataStart + fileData + fileDataEnd
     }
+
+    public paintHBS() {
+        const leaf = new BPSlot(this.output, this.projectName, this.routeName)
+
+        return `${leaf.paintShow()}{{yield}}`
+     }
 }
