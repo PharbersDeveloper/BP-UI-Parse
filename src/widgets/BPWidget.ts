8ad4f6f4d21a9353218fcc08465a13162e4517f5
frank <wangsen9108@gmail.com>
2020-02-25 14:34:55 +0800

    修改样式文件中出现的空样式以及 undefined：undefined 这样的错误样式。

--- src/widgets/BPWidget.ts
+++ src/widgets/BPWidget.ts
@@ -294,7 +294,14 @@ export abstract class BPWidget extends BPObject {
 
         const className: string = comp.className || comp.name
         const pointClass: string = prefix ? `${prefix} .${className} ` : `.${className} `
-        const styleProperties: CssProperty[] = [...comp.css, ...comp.layout]
+
+        // const styleProperties: CssProperty[] = [...comp.css, ...comp.layout]
+        // 因为在 BPApplication line 78 对来自 json 文件的 comp 的 css 以及 layout
+        // 属性进行了合并，并将处理后的数组放入了 BPComp 实例的 css 属性中，所以这里不再需要
+        // 获取实例的 layout 属性。因为这时实例的 layout 属性还是未经处理的 ParseCssConf
+        // 对象
+        const styleProperties: CssProperty[] = [...comp.css]
+
         if (styleProperties.length === 0) {
             return `\r`
         }
