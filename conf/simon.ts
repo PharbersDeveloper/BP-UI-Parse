b911aeae64066396738b28daadcce07dabf9b05b
fuhanyi <simonzoo@yeah.net>
2020-03-26 18:36:42 +0800

    新配置

---
+++ conf/simon.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    isAddon: false,
+    // outputPath: "output",
+    // 绝对路径写法
+    outputPath: "/Users/simon/Desktop/ui-output",
+    targetFile: "./test/data/radio/main.json",
+}
+
